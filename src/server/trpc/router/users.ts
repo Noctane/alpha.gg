af4e9b17c18a8b70dd3d33732c4ae23e494db5c6
Noctane <arnaud.prat@gmail.com>
2022-11-01 19:52:54 +0100

    feat: add user personal scores

--- src/server/trpc/router/users.ts
+++ src/server/trpc/router/users.ts
@@ -3,11 +3,17 @@ import { z } from "zod";
 
 export const usersRouter = router({
   getById: publicProcedure.input(z.number()).query(({ ctx, input }) => {
-    return ctx.prisma.user.findUnique({
+    const user = ctx.prisma.user.findUnique({
       where: {
         id: input,
       },
+      include: {
+        worldle: true,
+        sutom: true,
+      },
     });
+
+    return user;
   }),
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.user.findMany();

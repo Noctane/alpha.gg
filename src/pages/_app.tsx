a233f56c05d79b41bc6a4cd4f4210164d1a401d3
Noctane <arnaud.prat@gmail.com>
2022-11-01 20:20:20 +0100

    refactor: :fire: remove useless auth files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,10 @@
 // src/pages/_app.tsx
 import "../styles/globals.css";
-import { SessionProvider } from "next-auth/react";
-import type { Session } from "next-auth";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
 };
 
 export default trpc.withTRPC(MyApp);
--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -3,10 +3,8 @@ import { router } from "../trpc";
 import { usersRouter } from "./users";
 import { worldleRouter } from "./worldle";
 import { sutomRouter } from "./sutom";
-import { authRouter } from "./auth";
 
 export const appRouter = router({
-  auth: authRouter,
   users: usersRouter,
   worldle: worldleRouter,
   sutom: sutomRouter,
